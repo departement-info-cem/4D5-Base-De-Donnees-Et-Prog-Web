1093b2a2d8c8b3900464d6ac1a1a0e8b4fd2647b
Benoit Caldairou <benoit.caldairou@cegepmontpetit.ca>
2025-01-20 16:37:58 -0500

    Mise a jour de la description du cours

--- web/config.js
+++ web/config.js
@@ -1,6 +1,6 @@
 const config = {
     nom: "4D5 - Base de données et programmation web",
-    description: "Site de référence du cours de x",
+    description: "Site de référence du cours de Base de données et programmation web",
     nomUrl: "4D5-Base-De-Donnees-Et-Prog-Web"
 }
 
